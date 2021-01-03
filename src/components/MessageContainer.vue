9050b3df68a3aef129596a94384eeb741237015d
Andrew <a@fandb.pro>
2021-01-03 23:51:32 +0300

    Stubbing child components

---
+++ src/components/MessageContainer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <MessageDisplay />
+  </div>
+</template>
+
+<script>
+import MessageDisplay from '@/components/MessageDisplay'
+export default {
+  name: 'MessageContainer',
+  components: { MessageDisplay }
+}
+</script>
+
+<style scoped></style>
