a706359efc146308587cdd121a6a6a3977f864bc
Andrew <a@fandb.pro>
2021-01-03 22:12:17 +0300

    Testing emitting events

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <input type="text" v-model="name"/>
+    <button type="submit">Submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "LoginForm",
+  data: () => ({
+    name: ''
+  }),
+  methods: {
+    onSubmit() {
+      this.$emit('formSubmitted', {name: this.name})
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
